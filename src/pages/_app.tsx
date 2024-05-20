cad0a4835df65744b91a1295abff5ff981f7d442
jiohjung98 <ckaquz98@naver.com>
2024-05-21 05:03:35 +0900

    fix: main

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,16 +6,16 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Netflix</title>
-        <meta name="description" content="Netflix" />
+        <title>Netflix Project</title>
+        <meta name="description" content="A project demonstrating Next.js and React." />
         <link rel="icon" href="/netflix.png" />
-        <meta property="og:title" content="Netflix" />
-        <meta property="og:description" content="Netflix" />
+        <meta property="og:title" content="Netflix Project" />
+        <meta property="og:description" content="A project demonstrating Next.js and React." />
         <meta property="og:image" content="/netflix.png" />
         <meta property="og:url" content="https://netfilx-project.netlify.app/" />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="Netflix" />
-        <meta name="twitter:description" content="Netflix project" />
+        <meta name="twitter:title" content="Netflix Project" />
+        <meta name="twitter:description" content="A project demonstrating Next.js and React." />
         <meta name="twitter:image" content="/netflix.png" />
       </Head>
       <div className="full-page-background">
