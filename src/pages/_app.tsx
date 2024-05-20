574d3f13dc9259cd7a5acf541080504a008fac79
jiohjung98 <ckaquz98@naver.com>
2024-05-21 03:26:40 +0900

    feat: add meta tag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,26 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div className="full-page-background">
-      <Component {...pageProps} />
-    </div>
+    <>
+      <Head>
+        <title>Netflix</title>
+        <meta name="description" content="Your App Description" />
+        <link rel="icon" href="/netflix.png" />
+        <meta property="og:title" content="Your App Title" />
+        <meta property="og:description" content="Your App Description" />
+        <meta property="og:image" content="/netflix.png" />
+        <meta property="og:url" content="https://netfilx-project.netlify.app/" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Your App Title" />
+        <meta name="twitter:description" content="Your App Description" />
+        <meta name="twitter:image" content="/netflix.png" />
+      </Head>
+      <div className="full-page-background">
+        <Component {...pageProps} />
+      </div>
+    </>
   );
 }
