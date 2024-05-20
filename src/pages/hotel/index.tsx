54d2c825431174fa7ace7054662fd7cb45e3ecf0
jiohjung98 <ckaquz98@naver.com>
2024-05-21 04:30:41 +0900

    fix: main

--- src/pages/hotel/index.tsx
+++ src/pages/hotel/index.tsx
@@ -22,19 +22,19 @@ const HotelInformationPage = () => {
               <div className="flex justify-center w-full mx-auto gray-500 my-[2rem]  z-10">
                 <hr className='w-[30%] z-10'></hr>
               </div>
-              <p className="text-white mt-[1rem] mb-[1.5rem] font-extrabold z-10" style={{ fontSize: '22px' }}>CHECK-IN PROCESS</p>
+              <p className="text-white mb-[1.5rem] font-extrabold z-10" style={{ fontSize: '20px' }}>CHECK-IN PROCESS</p>
               <div className="netflix-light font-extrabold text-white my-auto mb-[1rem] z-10" style={{ fontSize: '16px' }}>Registration desk is located<br/>on the 1st floor of Mondrian Hotel</div>
               <div className="netflix-light font-extrabold text-white my-auto z-10" style={{ fontSize: '16px' }}>Registration desk opens in May 29th, 9:00AM</div>
               <div className="netflix-light text-white my-auto z-10" style={{ fontSize: '14px' }}>For early check-ins, please provide<br/>your name on the 2nd floor registration desk</div>
               <div className="flex justify-center w-full mx-auto gray-500 my-[2rem] z-10">
                 <hr className='w-[30%]'></hr>
               </div>
-              <p className="text-white mt-[1rem] mb-[1.5rem] font-extrabold z-10" style={{ fontSize: '22px' }}>HOTEL FACILITIES</p>
-                <div className="netflix-light text-white my-auto font-medium z-10" style={{ fontSize: '12px' }}>Gym</div>
+              <p className="text-white mb-[1.5rem] font-extrabold z-10" style={{ fontSize: '20px' }}>HOTEL FACILITIES</p>
+                <div className="netflix-light text-white my-auto font-bold z-10" style={{ fontSize: '12px' }}>Gym</div>
                 <div className="netflix-light text-white my-auto mb-[0.5rem] z-10" style={{ fontSize: '11px' }}>Mondrian Hotel, 5F |06:00 AM - 10:00 PM</div>
-                <div className="netflix-light text-white my-auto font-medium z-10" style={{ fontSize: '12px' }}>Swimming Pool</div>
+                <div className="netflix-light text-white my-auto font-bold z-10" style={{ fontSize: '12px' }}>Swimming Pool</div>
                 <div className="netflix-light text-white my-auto mb-[0.5rem] z-10" style={{ fontSize: '11px' }}>Mondrian Hotel, B2F | 06:00 AM</div>
-                <div className="netflix-light text-white my-auto font-medium z-10" style={{ fontSize: '12px' }}>Breakfast</div>
+                <div className="netflix-light text-white my-auto font-bold z-10" style={{ fontSize: '12px' }}>Breakfast</div>
                 <div className="netflix-light text-white my-auto mb-[0.5rem] z-10" style={{ fontSize: '11px' }}>Mondrian Hotel, 1F CLEO restaurant | 06:30 - 10:00 AM</div>
 
       </div>
