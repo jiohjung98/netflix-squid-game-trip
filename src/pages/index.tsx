7c518fd95a1a31aef1ff9d9f19a780737a4ee1f3
jiohjung98 <ckaquz98@naver.com>
2024-05-21 22:48:31 +0900

    feat: main

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,43 +7,46 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <div className="flex flex-col justify-between items-center h-screen">
-    <div className="w-full my-auto text-center"> 
+      <div className="w-[40%] mt-[5rem] text-center">
         <Image
-          src="/mainPhotos.png"
-          alt="mainPhotos"
+          src="/mainLogo.png"
+          alt="mainLogo"
           layout="responsive"
-          width={1920} 
-          height={300} 
+          width={200}
+          height={200}
           quality={100}
           priority
         />
       </div>
-      <div className="top-minus-value flex flex-col justify-between items-center z-10 t-">
-        <div className="my-auto text-center z-10">
+      <div className="w-full flex flex-col justify-center items-center flex-grow">
+        <div className="my-auto text-center z-10 flex flex-col justify-center items-center h-full">
           <div
-            className="netflix-light text-white my-auto mb-[1rem] font-light z-10"
+            className="netflix-light text-white font-light z-10 mb-[4rem]"
             style={{ fontSize: "14px" }}
           >
             Embark on an unforgettable
           </div>
           <p
             className="text-white mt-[1rem] mb-[1rem] font-black z-10"
-            style={{ fontSize: "30px" }}
+            style={{ fontSize: "32px" }}
           >
             NETFLIX JOURNEY
           </p>
           <div
-            className="netflix-light text-white my-auto mb-[1rem] z-10"
+            className="netflix-light text-white mt-[3rem] mb-[1rem] z-10"
             style={{ fontSize: "12px" }}
           >
             Join us for an exclusive journey through Korea,
             <br />
-            featuring an exciting lineup of activities<br />
-            that will leave you craving more.<br />
+            featuring an exciting lineup of activities
+            <br />
+            that will leave you craving more.
+            <br />
             Pack your bags and prepare for an incredible experience!
             <br />
             <br />
-            Upon arrival, you'll be warmly welcomed by a Netflix host, <br />
+            Upon arrival, you'll be warmly welcomed by a Netflix host,
+            <br />
             and we've arranged hotel transfers for your convenience.
           </div>
         </div>
@@ -51,7 +54,7 @@ export default function Home() {
 
       <div className="bottom-10 w-full flex justify-center items-center pb-[2.2rem] z-10">
         <Link href="/category">
-        <div
+          <div
             className="text-white mt-[1rem] mb-[1rem] font-black z-10"
             style={{ fontSize: "25px" }}
           >
