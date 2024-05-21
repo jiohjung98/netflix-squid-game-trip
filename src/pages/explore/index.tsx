1fe4cf9966b9d31aa4cbfb52676aa0172af4e5bb
jiohjung98 <ckaquz98@naver.com>
2024-05-21 22:17:04 +0900

    feat: other activities

--- src/pages/explore/index.tsx
+++ src/pages/explore/index.tsx
@@ -169,7 +169,7 @@ const ExploringPage = () => {
               <p className="text-white font-bold py-4 px-4 rounded-lg text-left mb-8">Things To Do Near Hotel</p>
             </div>
           </Link>
-          <Link href="/seoul">
+          <Link href="/otherActivities">
             <div className="block rounded-lg" style={{ backgroundColor: '#E50914' }}>
               <p className="text-white font-bold py-4 px-4 rounded-lg text-left mb-8">Other Activities</p>
             </div>
