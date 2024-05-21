b6b6b152d8d69c67baec230085b3bd70a5680567
jiohjung98 <ckaquz98@naver.com>
2024-05-22 00:53:56 +0900

    fix: weather

--- src/pages/weather/index.tsx
+++ src/pages/weather/index.tsx
@@ -9,9 +9,26 @@ const WeatherForcastPage = () => {
         <p className="text-white mt-[1rem] mb-[1rem] font-extrabold" style={{ fontSize: '25px' }}>WEATHER FORECAST</p>
         
         <div className='w-full flex flex-col justify-center items-center flex-grow'>
-          <div className="text-white mt-[1rem] mb-[1rem] font-medium" style={{ fontSize: '22px' }}>May 29 Wednesday<br/>16°C / 26°C (78°F)</div>
-          <div className="text-white mt-[1rem] mb-[1rem] font-medium" style={{ fontSize: '22px' }}>May 30 Thursday<br/>17°C / 26°C (77°F)</div>
-          <div className="text-white mt-[1rem] mb-[1rem] font-medium" style={{ fontSize: '22px' }}>May 31 Friday<br/>16°C / 26°C (78°F)</div>
+        <div className="text-white mt-[1rem] mb-[1rem] font-medium" style={{ fontSize: '16px' }}>Currently, the weather is generally<br/>clear and warm. In Seoul, the temperature<br/>is around 24°C (75°F) with a clear<br/>sky and light winds from the west.<br/>The humidity is moderate at about 59%</div>
+        <div className="text-white mt-[1rem] mt-[2rem] mb-[1rem] font-medium" style={{ fontSize: '22px' }}>
+          May 29 Wednesday<br/>
+          <span style={{ color: 'blue' }}>16°C (61°F)</span> 
+          <span style={{ color: 'white' }}> / </span> 
+          <span style={{ color: 'red' }}>27°C (81°F)</span>
+        </div>
+        <div className="text-white mt-[1rem] mb-[1rem] font-medium" style={{ fontSize: '22px' }}>
+          May 30 Thursday<br/>
+          <span style={{ color: 'blue' }}>17°C (63°F)</span> 
+          <span style={{ color: 'white' }}> / </span> 
+          <span style={{ color: 'red' }}>26°C (79°F)</span>
+        </div>
+        <div className="text-white mt-[1rem] mb-[1rem] font-medium" style={{ fontSize: '22px' }}>
+        May 31 Friday<br/>
+          <span style={{ color: 'blue' }}>16°C (61°F)</span> 
+          <span style={{ color: 'white' }}> / </span> 
+          <span style={{ color: 'red' }}>27°C (81°F)</span>
+        </div>
+          
         </div>
         
         {/* <div className="netflix-light text-white mb-[1.5rem]" style={{ fontSize: '14px' }}>Avg. daytime weather: 24°C (75.2°F)</div>
