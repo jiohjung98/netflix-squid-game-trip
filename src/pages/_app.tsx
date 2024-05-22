5095fe06d0732030656ce2f7ad628e9924a07c28
jiohjung98 <ckaquz98@naver.com>
2024-05-22 14:27:43 +0900

    fix: title, meta tag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,16 +6,16 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Netflix Project</title>
-        <meta name="description" content="A project demonstrating Next.js and React." />
+        <title>Netflix-Squid-Game-Trip</title>
+        <meta name="description" content="Netflix-Squid-Game-Trip" />
         <link rel="icon" href="/netflix.png" />
-        <meta property="og:title" content="Netflix Project" />
-        <meta property="og:description" content="A project demonstrating Next.js and React." />
+        <meta property="og:title" content="Netflix-Squid-Game-Trip" />
+        <meta property="og:description" content="Netflix-Squid-Game-Trip" />
         <meta property="og:image" content="/netflix.png" />
         <meta property="og:url" content="https://netfilx-project.netlify.app/" />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="Netflix Project" />
-        <meta name="twitter:description" content="A project demonstrating Next.js and React." />
+        <meta name="twitter:title" content="Netflix-Squid-Game-Trip" />
+        <meta name="twitter:description" content="Netflix-Squid-Game-Trip" />
         <meta name="twitter:image" content="/netflix.png" />
       </Head>
       <div className="full-page-background">
