3c6fcf943410cf0495bb952b63ea7216d63bb86f
jiohjung98 <ckaquz98@naver.com>
2024-05-23 16:23:39 +0900

    fix: google analytics

--- src/libs/gtag.ts
+++ src/libs/gtag.ts
@@ -1,26 +1,26 @@
 export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_ID;
 
-export const pageview = (url: URL | string) => {
-  if (process.env.NODE_ENV !== "development") {
-    window.gtag("config", GA_TRACKING_ID as string, {
-      page_path: url,
-    });
-  }
+// https://developers.google.com/analytics/devguides/collection/gtagjs/pages
+export const pageview = (url: URL) => {
+  if (typeof window.gtag == 'undefined') return;
+
+  window.gtag('config', GA_TRACKING_ID as string, {
+    page_path: url,
+  });
 };
 
-type gtagEvent = {
+interface GTagEventProps {
   action: string;
   category: string;
   label: string;
   value: number;
-};
+}
 
-export const event = ({ action, category, label, value }: gtagEvent) => {
-  if (process.env.NODE_ENV !== "development") {
-    window.gtag("event", action, {
-      event_category: category,
-      event_label: label,
-      value: value,
-    });
-  }
+// https://developers.google.com/analytics/devguides/collection/gtagjs/events
+export const event = ({ action, category, label, value }: GTagEventProps) => {
+  window.gtag('event', action, {
+    event_category: category,
+    event_label: label,
+    value,
+  });
 };
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,39 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Head from 'next/head';
+import Script from 'next/script'
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+import * as gtag from '@/libs/gtag';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = (url: URL) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return (
     <>
       <Head>
+      <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', '${gtag.GA_TRACKING_ID}', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        />
         <title>Netflix-Squid-Game-Trip</title>
         <meta name="description" content="Netflix-Squid-Game-Trip" />
         <link rel="icon" href="/netflix.png" />
@@ -18,6 +46,10 @@ export default function App({ Component, pageProps }: AppProps) {
         <meta name="twitter:description" content="Netflix-Squid-Game-Trip" />
         <meta name="twitter:image" content="/netflix.png" />
       </Head>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
       <div className="full-page-background">
         <Component {...pageProps} />
       </div>
