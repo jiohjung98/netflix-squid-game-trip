c847bc46738a6c71fb880fa7d797409966ab07b7
jiohjung98 <ckaquz98@naver.com>
2024-05-23 16:38:01 +0900

    fix: google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,6 +6,8 @@ import { useRouter } from 'next/router';
 import { useEffect } from 'react';
 import * as gtag from '@/libs/gtag';
 
+const GA_TRACKING_ID = 'G-58LZ0H6Y0L';
+
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter()
   useEffect(() => {
@@ -21,19 +23,24 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-      <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-
-              gtag('config', '${gtag.GA_TRACKING_ID}', {
-                page_path: window.location.pathname,
-              });
-            `,
-          }}
-        />
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
         <title>Netflix-Squid-Game-Trip</title>
         <meta name="description" content="Netflix-Squid-Game-Trip" />
         <link rel="icon" href="/netflix.png" />
@@ -45,11 +52,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <meta name="twitter:title" content="Netflix-Squid-Game-Trip" />
         <meta name="twitter:description" content="Netflix-Squid-Game-Trip" />
         <meta name="twitter:image" content="/netflix.png" />
-      </Head>
-      <Script
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
-      />
+      </Head>      
       <div className="full-page-background">
         <Component {...pageProps} />
       </div>
