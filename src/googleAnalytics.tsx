c6be4ee6e37982872f359fafd1c2fbe91560af45
jiohjung98 <ckaquz98@naver.com>
2024-05-23 15:14:09 +0900

    feat: google analytics

---
+++ src/googleAnalytics.tsx
@@ -0,0 +1,23 @@
+'use client';
+
+import Script from 'next/script';
+
+const GA_MEASUREMENT_ID = 'G-58LZ0H6Y0L';
+
+export default function GoogleAnalytics() {
+  return (
+    <>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+      />
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${GA_MEASUREMENT_ID}');
+        `}
+      </Script>
+    </>
+  );
+}
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,8 +1,10 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import GoogleAnalytics from '@/googleAnalytics';
 
 export default function Document() {
   return (
     <Html lang="en">
+      <GoogleAnalytics />
       <Head />
       <body>
         <Main />
